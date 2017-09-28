a9b42a18c259a4888f3b25e6ba35af2df4c20af8
to-explore-future <869518570@qq.com>
2017-09-28 09:04:08 +0800

    1.AsyncTash练习2.排序练习3.

--- src/main/java/scut/carson_ho/main/MainActivity.java
+++ src/main/java/scut/carson_ho/main/MainActivity.java
@@ -1,12 +1,16 @@
 package scut.carson_ho.main;
 
+import android.content.ComponentCallbacks2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.util.Log;
 import android.view.View;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import scut.carson_ho.main.AsynTask.AsyncTaskActivity;
 import scut.carson_ho.main.Bar.BarActivity;
 import scut.carson_ho.main.ContentProvider.ContentProviderActivity;
 import scut.carson_ho.main.UI.UIActivity;
@@ -16,12 +20,16 @@ import scut.carson_ho.main.sqlite.SQLiteActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivityss";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activiyt);
         ButterKnife.bind(this);
-
+        Log.i(TAG, "onCreate:");
+        bubble();
     }
 
     @OnClick({R.id.btn_about_service, R.id.btn_about_broadcast, R.id.btn_about_sqlite, R.id.btn_ui
@@ -47,10 +55,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, BarActivity.class));
                 break;
             case R.id.btn_asyncTask:
-                
+                startActivity(new Intent(this, AsyncTaskActivity.class));
                 break;
         }
     }
 
+    int a[] = {2, 78, 100, 34, 56, 78, 89, 23, 0, 40};
+    int len = a.length;
+    /**
+     * 冒泡排序：
+     */
+    private void bubble() {
 
+
+        int m = 0;
+        for (int i = 0; i < len; i++) {
+
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    m = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = m;
+                }
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            Log.i(TAG, a[i] + "");
+        }
+    }
+
+    /**
+     * 选择排序：每次从数组中选择一个最小值，放到指定的位置
+     */
+    private void selectionSort() {
+        for (int i = 0; i < len; i++) {
+
+        }
+    }
+
+    /**
+     * 应用在后台运行 也是占用着内存，
+     *
+     * @param level
+     */
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Log.i(TAG, "onTrimMemory:");
+        if (level == ComponentCallbacks2.TRIM_MEMORY_COMPLETE) {
+
+        } else if (level == ComponentCallbacks2.TRIM_MEMORY_MODERATE) {
+
+        } else if (level == ComponentCallbacks2.TRIM_MEMORY_BACKGROUND) {
+
+        } else if (level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
+            /**
+             * the process had been showing a user interface.and is no longer doing so,
+             * large allocation with UI should be released at this point to allow memory to be better maneged.
+             */
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        Log.i(TAG, "onLowMemory:");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy:");
+    }
 }
